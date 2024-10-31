96da9c400c4d714152b06879e8450d41dab8c6ff
itznishantthapa <itsnishantu@gmail.com>
2024-10-31 19:27:48 +0545

    Read SetUp for the Setting Screens

---
+++ components/profile/ProfileUpdation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProfileUpdation = () => {
+  return (
+    <View>
+      <Text>ProfileUpdation</Text>
+    </View>
+  )
+}
+
+export default ProfileUpdation
+
+const styles = StyleSheet.create({})
--- components/profile/UserInfo.tsx
+++ components/profile/UserInfo.tsx
@@ -3,15 +3,20 @@ import React from 'react'
 import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons'
 import { Image } from 'react-native'
 import { scaleHeight, scaleWidth } from '../../Scaling'
+import  FontAwe  from 'react-native-vector-icons/FontAwesome6'
+import { styles } from '../../style/style'
 
 
 const UserInfo = () => {
     return (
         <View style={ownstyle.profileImageContainer}>
-            <Image
-                source={require('../../assets/chatapate.jpeg')}
-                style={ownstyle.profileImage}
-            />
+           
+                <Image
+                    source={require('../../assets/images/profile.webp')}
+                    style={ownstyle.profileImage}
+                />
+            
+
             <View style={{ alignItems: 'center', gap: scaleHeight(4) }}>
                 <Text style={ownstyle.nameText}>Nishant Thapa</Text>
                 <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: scaleWidth(5) }}>
@@ -32,11 +37,13 @@ const ownstyle = StyleSheet.create({
     profileImage: {
         width: scaleWidth(100),
         height: scaleHeight(100),
-        borderRadius:scaleWidth(75),
+        borderRadius: scaleWidth(75),
+        // backgroundColor: 'black',
     },
-    nameText:{
-      fontFamily:'poppins_regular'
-    }
+    nameText: {
+        fontFamily: 'poppins_regular'
+    },
+  
 })
 
 
